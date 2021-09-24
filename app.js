fffe385b76be12938ec3f8f756d2f1f7bc9e8472
Bibin Antony <40894472+Bibin-Antony@users.noreply.github.com>
2021-09-24 20:32:18 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var btnTranslate = document.getElementById("btn-translate");
 var txtInput = document.querySelector("#txt-input");
 var outputBox = document.querySelector("#output-box");
 
-var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+var serverURL = "https://api.funtranslations.com/translate/minion.json";
 
 btnTranslate.addEventListener("click", clickEventHandler);
 
